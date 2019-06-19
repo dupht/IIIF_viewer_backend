239d7972e21f51c9d93b23755990f38cdd05c2d2
dupht <hanteru1999@gmail.com>
2019-06-19 12:41:03 +0900

    app.js com

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 IIIF_viewer in COJT, part of backend
 
 Express Node.js
+
+pick_image.jsは手打ちでURLを指定すると画像URLを配列に入れてコンソールに表示してくれる
+
+app.jsを起動しておくと http://localhost:3000/ で待機する。Manifest.jsonのURLをHTTP-POST-Requestで投げると画像URLを配列に入れて返してくれる。
--- app.js
+++ app.js
@@ -7,20 +7,25 @@ const manifest_url = "https://www.dl.ndl.go.jp/api/iiif/8929985/manifest.json";
 const images = [];
 
 
-app.get("/", function (manifest_url, res){
-    fetch(manifest_url)
-    .then(res => res.json())
-    .then(response => {
-        console.log('Success!');
-        response.sequences[0].canvases.forEach(element => {
-            images.push(element.thumbnail["@id"]);
-        });
-    })
-    .then(()=>{
-        res.send(images);
-        console.log(images);
-    })
-    .catch(error => console.error('Error:', error));    
+app.get("/", function (req, res){
+    const manifest_url = req.body;
+    if( manifest_url.match(/http/) ){
+        fetch(manifest_url)
+        .then(res => res.json())
+        .then(response => {
+            console.log('Success!');
+            response.sequences[0].canvases.forEach(element => {
+                images.push(element.thumbnail["@id"]);
+            });
+        })
+        .then(()=>{
+            res.send(images);
+            console.log(images);
+        })
+        .catch(error => console.error('Error:', error));
+    }else{
+        res.send("request is only allowed absolutely path to manifest.json");
+    }
 });
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
