2216c4c4f437170a103755ecaee749bd30564aec
dupht <hanteru1999@gmail.com>
2019-06-28 16:16:08 +0900

    fetch-node fin

---
+++ search_manifest.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const fetch = require('node-fetch');
+const Bluebird = require('bluebird');
+const { URLSearchParams } = require('url');
+
+fetch.Promise = Bluebird;
+
+let api_key = process.env.EUROPEAN_API_KEY;
+let url = new URL("https://www.europeana.eu/api/v2/search.json");
+let params = {query:"sv_dcterms_conformsTo:*iiif*", rows:"10", wskey: api_key}
+url.search = new URLSearchParams(params);
+
+
+fetch( url )
+.then( res => res.json() )
+.then( response => console.log( 'Success:', JSON.stringify(response) ) )
+.catch( error => console.error( 'Error:', error ) );
