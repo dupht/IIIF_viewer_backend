871ff4baaa9d9aefb94de342a1d0518230d15fed
dupht <hanteru1999@gmail.com>
2019-07-20 14:50:04 +0900

    非同期処理に手間取った舐めてた

---
+++ rest_api_for_search_manifest.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const search_manifest = require("./search_manifest");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", async (req, res, next) => {
+
+    (async () => {
+        let request = req.body;
+
+        let list = await search_manifest.get_manifest(request.query, request.theme, request.sort, request.rows);
+        console.log(JSON.stringify(list));
+        res.status(200).json(JSON.stringify(list));
+
+    })().catch(next);
+
+});
+
+app.listen(3000, () => console.log("listen on port 3000"));
--- search_manifest.js
+++ search_manifest.js
@@ -6,30 +6,26 @@ const { URLSearchParams } = require('url');
 fetch.Promise = Bluebird;
 
 
-function get_manifest( query ){
+exports.get_manifest = async function( req_query, req_theme, req_sort, req_rows ){
     let api_key = process.env.EUROPEAN_API_KEY;
     let url = new URL("https://www.europeana.eu/api/v2/search.json");
-    let params = {query:"sv_dcterms_conformsTo:*iiif*", rows:"10", wskey: api_key, qf: "title:*"+ query +"*"};
+    let params = {query:"sv_dcterms_conformsTo:*iiif*", rows: req_rows, wskey: api_key, qf: "title:*"+ req_query +"*", theme: req_theme, sort: req_sort };
     url.search = new URLSearchParams(params);
-
-
-    fetch( url )
+    
+    return fetch( url )
     .then( res => res.json() )
     .then( response => {
         console.log( 'Success:');
         manifest_array = [];
-        var i=0;
         response.items.forEach( function(value) {
             manifest_array.push({
                 "url" : "https://iiif.europeana.eu/presentation" + value.id + "/manifest.json",
                 "title" : value.title[0],
-                "description" : value.dcDescription[0]
+                "description" : value.dcDescription[0],
+                "thumbnail": ""
             });
-            console.log(manifest_array[i]);
-            i++;
         });
+        return manifest_array;
     })
     .catch( error => console.error( 'Error:', error ) );
 }
-
-get_manifest("北斎");
