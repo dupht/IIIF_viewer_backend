ad758086662da203fb2dc53c80f254a058dbb620
dupht <hanteru1999@gmail.com>
2019-07-25 17:22:03 +0900

    make sure

--- search_manifest_twice.js
+++ search_manifest_twice.js
@@ -5,8 +5,21 @@ const { URLSearchParams } = require('url');
 
 fetch.Promise = Bluebird;
 
+search_manifest_url = async function( id ){
+    let url = new URL("https://www.europeana.eu/api/v2/record" + id + ".json");
+    let params = { wskey: process.env.EUROPEAN_API_KEY }
+    url.search = new URLSearchParams(params);
 
-exports.get_manifest = async function( req_query, req_theme, req_sort, req_rows ){
+    return fetch( url )
+    .then(res => res.json() )
+    .then(response => {
+        response.
+    })
+    .catch(error => console.error( 'Error:', error ));
+}
+
+
+get_manifest = async function( req_query, req_theme, req_sort, req_rows ){
     let api_key = process.env.EUROPEAN_API_KEY;
     let url = new URL("https://www.europeana.eu/api/v2/search.json");
     let params = {query:"sv_dcterms_conformsTo:*iiif*", rows: req_rows, wskey: api_key, qf: "title:*"+ req_query +"*", theme: req_theme, sort: req_sort };
@@ -17,10 +30,10 @@ exports.get_manifest = async function( req_query, req_theme, req_sort, req_rows 
     .then( response => {
         console.log( 'Success:');
         manifest_array = [];
-        response.items.forEach( function(value) {
-
+        response.items.forEach( async function(value) {
+            manifest_url = await search_manifest_url(vlaue.id);
             manifest_array.push({
-                "url" : "https://iiif.europeana.eu/presentation" + value.id + "/manifest.json",
+                "url" : manifest_url,
                 "title" : value.title[0],
                 "description" : value.dcDescription[0],
                 "thumbnail": value.edmPreview[0],
@@ -31,3 +44,5 @@ exports.get_manifest = async function( req_query, req_theme, req_sort, req_rows 
     })
     .catch( error => console.error( 'Error:', error ) );
 }
+
+console.log( get_manifest("北斎", "", "", "10") );
